b99526d9082d8473fd21f418c18fe46669377ee5
SanjanaPurma <AIUMFS7@amway.com>
2022-08-19 12:12:32 +0530

    include-check-box

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,21 +1,29 @@
 import React from "react";
 
 const ToDo = ({ todo, handleToggle }) => {
+  const [checked, setChecked] = React.useState(todo.complete);
   const handleClick = (e) => {
+    setChecked(e.currentTarget.checked);
     e.preventDefault();
     handleToggle(e.currentTarget.id);
   };
 
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <div
-      id={todo.id}
-      key={todo.id + todo.task}
-      name="todo"
       value={todo.id}
+      key={todo.id + todo.task}
+      id={todo.id}
+      name="todo"
       onClick={handleClick}
-      className={todo.complete ? "todo strike" : "todo"}
     >
-      {todo.task}
+      <input type="checkbox" checked={checked} onChange={handleChange} />
+      <label className={todo.complete ? "todo strike" : "todo"}>
+        {todo.task}
+      </label>
     </div>
   );
 };
