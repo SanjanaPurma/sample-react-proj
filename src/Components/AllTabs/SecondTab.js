c0226ccf4578fb8d737aeae4d78594c6d8404542
SanjanaPurma <AIUMFS7@amway.com>
2022-08-22 17:51:44 +0530

    included-drop-downs

--- src/Components/AllTabs/SecondTab.js
+++ src/Components/AllTabs/SecondTab.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import DropDown from "../Dorpdown/DropDown";
+
 const SecondTab = () => {
+  const teamActions = [
+    { label: "Interact", value: "interact" },
+    { label: "Brainstorm", value: "brainstorm" },
+    { label: "Review", value: "review" },
+  ];
+
+  const plannedActions = [
+    { label: "Sprint Planning", plannedValue: "Sprint Planning" },
+    { label: "Knowlege Sharing", plannedValue: "Knowlege Sharing" },
+    { label: "Restrospective", plannedValue: "Restrospective" },
+  ];
+
+  const [value, setValue] = useState("interact");
+  const [plannedValue, setPlannedValue] = useState("Sprint Planning");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const handlePlannedValueChange = (event) => {
+    setPlannedValue(event.target.value);
+  };
+
   return (
     <div className="SecondTab">
-      <p>Second Tab!! Hurray!!</p>
-      {/* Second  tab content will go here */}
+      <DropDown
+        label="What can we do?"
+        options={teamActions}
+        value={value}
+        onChange={handleChange}
+      />
+      <p>We can {value}!</p>
+      <DropDown
+        label="What can we plan?"
+        options={plannedActions}
+        value={plannedValue}
+        onChange={handlePlannedValueChange}
+      />
+      <p>We can plan {plannedValue}!</p>
     </div>
   );
 };
